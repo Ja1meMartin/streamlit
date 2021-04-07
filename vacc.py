633784ae4ee2445404cf2de31c4db5d5a862099d
Ja1meMartin <Jaime.Martin@Student.gcd.ie>
2021-04-07 20:30:26 +0100

    Requirements and Update
    - adjust future dates
    - target vacc percentage
    - filter countries
    - download csv

--- vacc.py
+++ vacc.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import base64
 from datetime import datetime
 
 now = datetime.now()
@@ -18,8 +19,8 @@ def diff(window):
 def recent_days(df, days):
     return df[df.date > datetime.now() - pd.to_timedelta(f'{days}day')]
 
-def add_future_dates(df):
-    ftr =  (df['date'] + pd.Timedelta(56, unit='days')).to_frame()
+def add_future_dates(df, days_in_future=56):
+    ftr =  (df['date'] + pd.Timedelta(days_in_future, unit='days')).to_frame()
     ftr["tag"] = "future"
     ftr=ftr.assign(
         diff1=recent_days(df,10).groupby(level=0)["diff1"].max(),
@@ -36,6 +37,12 @@ def add_future_dates(df):
     
     return df1
 
+first_dose_efficacy = st.sidebar.slider("Percentage of vaccination from first dose", 0.2, 1.0, 0.8, 0.01)
+first_dose_delay = st.sidebar.slider("How many days before first dose takes affect", 0, 21, 7, 1)
+second_dose_days = st.sidebar.slider("How many days does the second dose need before maximum efficacy", 7, 14, 7, 1)
+days_in_future = st.sidebar.slider("How many days in the future to add? Default 56", 0, 100, 56, 1)
+target_perc = st.sidebar.slider("Target vaccination percentage? Default 67 because Ireland is 3.6/4.8m. Vaccinations will be normalised to this number", 60, 100, 67, 1)
+
 @st.cache
 def load_df(y,m,h,d):
     """load the data every hour"""
@@ -50,14 +57,20 @@ def load_df(y,m,h,d):
         diff2=df.groupby(level=0)[second_dose].apply(diff)
     )
 
-    df = add_future_dates(df).reset_index().set_index("location")
+    
     return df
 
 df = load_df(now.year, now.month, now.day, now.hour)
 
-first_dose_efficacy = st.sidebar.slider("Percentage of vaccination from first dose", 0.2, 1.0, 0.8, 0.01)
-first_dose_delay=st.sidebar.slider("How many days before first dose takes affect", 0, 21, 7, 1)
-second_dose_days=st.sidebar.slider("How many days does the second dose need before maximum efficacy", 7, 14, 7, 1)
+countries = st.multiselect("Choose locations:", list(set(df.index)), 
+"Ireland,Northern Ireland,Israel,Chile,European Union,United States".split(","))
+
+if countries:
+    df = df[df.index.isin(countries)]
+
+df = add_future_dates(df, days_in_future).reset_index().set_index("location")
+
+
 
 def score1(ls):
     return sum((first_dose_efficacy/(28-first_dose_delay)) * vaccines 
@@ -78,18 +91,19 @@ df = df.reset_index().assign(
 df['daily1'].replace(np.nan, 0, inplace=True)
 df['daily2'].replace(np.nan, 0, inplace=True)
 df = df.assign(daily_total=df.daily1 + df.daily2)
-df = df.assign(rolling_score=df.groupby("location").daily_total.cumsum())
+df = df.assign(rolling_score=df.groupby("location").daily_total.cumsum() * (100/target_perc))
 df = df.assign(rolling_score=np.where(df.rolling_score <=100, df.rolling_score, 100))
 df = df.assign(weekly=df.groupby("location")['rolling_score'].apply(lambda window: (window - window.shift(7)).replace(np.nan, 0)))
 
 
-countries = st.multiselect("Choose locations:", list(set(df.location)), 
-"Ireland,Northern Ireland,Israel,Chile,European Union,United States".split(","))
-
-if countries:
-    df = df[df.location.isin(countries)]
-
-
-fig = px.line(df, x="date", y="rolling_score", color='location')
+fig = px.line(df, x="date", y="rolling_score", color='location', line_dash="tag")
 
 st.plotly_chart(fig)
+
+csv = df.to_csv().encode()
+
+b64 = base64.b64encode(csv).decode()
+
+href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+
+st.markdown(href, unsafe_allow_html=True)
